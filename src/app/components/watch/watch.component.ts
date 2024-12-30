0c137fac24fd9b3565b115201d187caf0ead7a3f
Jarvis <home80jarvis@gmail.com>
2024-12-30 12:06:12 +0530

    Auto full scrreen

--- src/app/components/watch/watch.component.ts
+++ src/app/components/watch/watch.component.ts
@@ -55,5 +55,12 @@ export class WatchComponent implements OnInit, AfterViewInit, OnDestroy {
     if (iframe) {
       iframe.style.opacity = '1';
     }
+    this.requestFullscreen(iframe)
   };
+
+  requestFullscreen(iframe: HTMLIFrameElement) {
+    if (iframe.requestFullscreen) {
+      iframe.requestFullscreen();
+    }
+  }
 }
