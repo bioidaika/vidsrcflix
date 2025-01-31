9d656e3c337e541ddfe08c9348c4d8a31e196d1b
Jarvis <home80jarvis@gmail.com>
2025-01-31 13:37:18 +0530

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,10 @@
 self.addEventListener("fetch", (event) => {
-    console.log("Request from iframe:", event.request.url);
-    fetch(event.request).then(response => {
-        console.log("Response", response);
-    });
+    console.log("Intercepted Request URL:", event.request.url);
+    
+    // Check if the request is an iframe resource (can add filtering logic here)
+    if (event.request.url.includes("iframe")) {
+        console.log("Intercepted iframe request:", event.request.url);
+    }
+    
+    event.respondWith(fetch(event.request));
 });
